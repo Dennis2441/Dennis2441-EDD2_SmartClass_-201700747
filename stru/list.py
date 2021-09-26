0fc2dfc98d81375a3976c0add75d30915fde0d3e
Dennis2441 <58497237+Dennis2441@users.noreply.github.com>
2021-09-26 11:23:36 -0600

    Commit 4

--- stru/list.py
+++ stru/list.py
@@ -479,6 +479,7 @@ class matrizx:
                             MapaRuta.write('<td>' + "\n")
                             MapaRuta.write("Matriz" + "\n")
                             MapaRuta.write('</td>' + "\n")
+                            MapaRuta.write('</td>' + "\n")
 
 
                         else:
